82df8c2385797c455739510bd28395979ca68b85
FrexCheat <frexcheat@163.com>
2024-06-08 19:25:26 +0800

    [Fix]: Fix the function of sub and events detail push.

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,6 @@ export interface mBot {
   // sandbox
   sandboxBot: Bot<Context>
 }
-declare const mbot: mBot
 export interface Webhook {
   id: string
   platform: string
@@ -44,35 +43,18 @@ export const Config: Schema<Config> = Schema.object({
   path: Schema.string().default('/webhook').description('输入你的Github Webhook payload路由路径'),
 })
 
-function sendCombineMessage(groupArray: Array<Webhook>, content: string) {
-  groupArray.forEach(GroupObj => {
-    let obot: Bot<Context>
-    switch (GroupObj.platform) {
-      case 'qq': obot = mbot.qqBot; break
-      case 'qqguild': obot = mbot.qqguildBot; break
-      case 'onebot': obot = mbot.oneBot; break
-      case 'red': obot = mbot.redBot; break
-      case 'telegram': obot = mbot.telegramBot; break
-      case 'satori': obot = mbot.satoriBot; break
-      case 'chronocat': obot = mbot.chronocatBot; break
+function sendEventMessage(_ctx: Context, groupArray: Array<Webhook>, content: string) {
+  groupArray.forEach(obj => {
+    try {
+      _ctx.broadcast([`${obj.platform}:${obj.id}`], content)
     }
-    obot.sendMessage(GroupObj.id, content)
+    catch (e) {
+      _ctx.logger('webhook-lite').warn(e)
+    }
   })
 }
 
 export function apply(ctx: Context, config: Config) {
-  ctx.bots.forEach(bot => {
-    switch (bot.platform) {
-      case 'qq': mbot.qqBot = bot; break
-      case 'qqguild': mbot.qqguildBot = bot; break
-      case 'onebot': mbot.oneBot = bot; break
-      case 'red': mbot.redBot = bot; break
-      case 'telegram': mbot.telegramBot = bot; break
-      case 'satori': mbot.satoriBot = bot; break
-      case 'chronocat': mbot.chronocatBot = bot; break
-    }
-  })
-
   ctx.model.extend('webhook', {
     id: 'string',
     platform: 'string',
@@ -87,7 +69,7 @@ export function apply(ctx: Context, config: Config) {
         await ctx.database.upsert('webhook', (row) => [
           { id: groupId, platform: platform },
         ])
-        session.send('本群订阅成功！')
+        session.send('本群订阅成功')
       }
       else {
         session.send('本插件仅限群聊使用！')
@@ -127,19 +109,18 @@ export function apply(ctx: Context, config: Config) {
     const star = repo.stargazers_count
     if (event === 'star') {
       if (payload.action === 'created') {
-        const content = `用户 <${sender['login']}> star 仓库 <${repo['full_name']}> (共计 ${star} 个star)`
+        const content = `用户 -${sender['login']}- star 仓库 -${repo['full_name']}- (共计 ${star} 个star)`
         const query = await ctx.database.get('webhook', {}) as Array<Webhook>
-        sendCombineMessage(query, content)
+        sendEventMessage(ctx, query, content)
       }
       if (payload.action === 'deleted') {
-        const content = `用户 <${sender['login']}> unstar 仓库 <${repo['full_name']}> (剩余 ${star} 个star)`
+        const content = `用户 -${sender['login']}- unstar 仓库 -${repo['full_name']}- (剩余 ${star} 个star)`
         const query = await ctx.database.get('webhook', {}) as Array<Webhook>
-        sendCombineMessage(query, content)
+        sendEventMessage(ctx, query, content)
       }
     }
-    //------------------------------------------//
+
     res.status = 200
     res.body = 'Webhook received'
-    //------------------------------------------//
   })
 }
